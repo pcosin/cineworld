d319cea21912b5c80f8bda1d3d0a097dfef19dcf
Pablo Cosin <pcosin@gmail.com>
2022-10-15 17:33:43 -0300

    still working contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -25,7 +25,55 @@ const botonEnviar = document.querySelector("[data-btn-contacto]");
 
 const validateForm = (formSelector) => {
   const formElement = document.querySelector(formSelector);
-  console.log("hola");
+
+  const validationOptions = [
+    {
+      attribute: 'required',
+      isValid: input => input.value.trim() !== '',
+      errorMessage: (input, label) => `${label.textContent} is required`,
+    },
+
+  ]
+
+  const validateSingleFormGroup = formGroup => {
+    const label = formGroup.querySelector('label');
+    const input = formGroup.querySelector('input, textarea');
+    const errorContainer = formGroup.querySelector('.error');
+    const errorIcon = formGroup.querySelector('.error-icon');
+    const successIcon = formGroup.querySelector('.success-icon');
+
+    let formGroupError = false;
+    for (const option of validationOptions) {
+      if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
+        errorContainer.textContent = option.errorMessage(input, label);
+        formGroupError = true;
+
+      }
+    }
+    console.log(label, input)
+
+    if (!formGroupError) {
+      errorContainer.textContent = '';
+      
+    }
+  
+  }
+
+
+  formElement.setAttribute('novalidate', '')
+  formElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+    validateAllFormGroups(formElement)
+  })
+
+  const validateAllFormGroups = (formToValidate) => {
+    const formGroups = Array.from(formToValidate.querySelectorAll('.form-item'))
+    
+    formGroups.forEach(formgroup => {
+      validateSingleFormGroup(formgroup)
+    })
+  }
+
 };
 
 validateForm("[data-form]");
