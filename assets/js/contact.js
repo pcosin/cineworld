b41575c1d450058cf0d0e32d1fb19525b7fb8200
pcosin <74868663+pcosin@users.noreply.github.com>
2022-10-15 09:18:26 -0300

    working with contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,19 +1,31 @@
 const botonEnviar = document.querySelector("[data-btn-contacto]");
 
-botonEnviar.addEventListener("submit", (e) => {
-  e.preventDefault();
+// const expresiones = {
+//   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+//   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+// };
 
-  const name = e.target.nombre.value;
+// botonEnviar.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  console.log("name");
+//   const name = e.target.nombre.value;
 
-  Email.send({
-    Host: "smtp.yourisp.com",
-    Username: "username",
-    Password: "password",
-    To: "them@website.com",
-    From: "you@isp.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-});
+//   console.log("name");
+
+//   Email.send({
+//     Host: "smtp.yourisp.com",
+//     Username: "username",
+//     Password: "password",
+//     To: "them@website.com",
+//     From: "you@isp.com",
+//     Subject: "This is the subject",
+//     Body: "And this is the body",
+//   }).then((message) => alert(message));
+// });
+
+const validateForm = (formSelector) => {
+  const formElement = document.querySelector(formSelector);
+  console.log("hola");
+};
+
+validateForm("[data-form]");
