b5d6467dd0d39aed51e5483ca9fa5af4ef25bb39
pcosin <pcosin@hotmail.com>
2023-10-12 10:22:18 -0300

    correcion de errores

--- assets/js/peliculas-desarrollo.js
+++ assets/js/peliculas-desarrollo.js
@@ -1,36 +1,37 @@
 const peliculasDesarrollo = [
   {
     id: 1,
+    title: "22 Preguntas",
+    img: "/assets/images/22-preguntas/22-preguntas.webp",
+    route: "22-preguntas.html",
+  },
+  {
+    id: 2,
     title: "Una semana en Madrid",
     img: "/assets/images/una-semana-en-madrid/una-semana-en-madrid.webp",
     description: "",
     route: "una-semana-en-madrid.html",
   },
   {
-    id: 2,
+    id: 3,
     title: "Mudanza internacional",
     img: "/assets/images/mundanza-internacional/mundanza-internacional.webp",
     description: "",
     route: "mudanza-internacional.html",
   },
   {
-    id: 3,
+    id: 4,
     title: "Noches blancas",
     img: "/assets/images/noches-blancas/noches-blancas.webp",
     description: "",
     route: "noches-blancas.html",
   },
   {
-    id: 4,
+    id: 5,
     title: "Las tres vidas de Carlos Slepoy",
     img: "/assets/images/carlos/carlos.webp",
     route: "carlos.html",
   },
-  {
-    id: 5,
-    title: "22 Preguntas",
-    img: "/assets/images/22-preguntas/22-preguntas.webp",
-    route: "22-preguntas.html",
-  },
+  
   
 ];
